e7ea2f170cee8ed90e814211b26cdcfaef64f644
Anil <ay5741432@gmail.com>
2026-04-11 15:22:59 +0530

    Day 3 updating the second assignment

--- 03_calculations/assi2.py
+++ 03_calculations/assi2.py
@@ -1,1 +1,16 @@
-i
+'''Assignment 2: Salary Calculator
+
+Write a Python program that:
+
+Accepts daily wage and number of days.
+Calculates total salary.
+
+Input:
+Daily wage = 500
+Days = 26
+
+Output:
+Salary = 13000'''
+
+wage, days = map(float,input("Enter Daily wage and Number of Days : ").split())
+print(f"Salary : {wage*days}")
