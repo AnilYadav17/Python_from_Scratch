52985c7bb45be276be1bdb2da769b9d3ebd7264b
Anil Yadav <ay5741432@gmail.com>
2026-04-12 11:03:32 +0530

    Compliting Assi14 in Day3

--- 03_calculations/assi14.py
+++ 03_calculations/assi14.py
@@ -16,4 +16,11 @@ Profit % = 20.0'''
 cost_price,selling_price= map(float,input("Enter Cost Price and Selling Price (use comma to separate) :").split(","))
 if(cost_price>selling_price):
  loss = cost_price-selling_price
- print("Loss% :",cost_price(loss/100))
+ print("Loss\t:",loss)
+ print("Loss%\t:",(loss/cost_price)*100)
+elif cost_price<selling_price:
+ profit = selling_price - cost_price
+ print("Profit\t:",profit)
+ print("Profit%\t:",(profit/cost_price)*100)
+else:
+ print("No Profit , No Loss")
