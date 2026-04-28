e9e3e1560f309cd8ec66476d1f63e0316f6e4c2e
Anil Yadav <ay5741432@gmail.com>
2026-04-28 22:29:33 +0530

    Adding Assi2 to 13 Day Calculations Branch

--- 13_calculations/assi2.py
+++ 13_calculations/assi2.py
@@ -2,15 +2,17 @@ import math
 num = int(input("Enter Number = "))
 num = num+1
 
-while True:
-    i=2
-    for i in range(i,(num//2)+1):
-        if num%i==0:
-            break
-        else:
-            print(num)
-    num+=1
-    break
 
+while True:
+    flag=0
+    i=2
+    for i in range(i,int(math.sqrt(num))+1):
+        if num%i==0:
+            flag=1
+            break
+    if flag==0:
+           print(num)
+           break
+    num+=1
  
 
