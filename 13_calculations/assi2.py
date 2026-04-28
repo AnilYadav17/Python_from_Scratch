c173076c10d6b2926278d62021a7f02dedca561d
Anil Yadav <ay5741432@gmail.com>
2026-04-28 19:28:11 +0530

    Adding remaining  Assignments in 13_calculations

--- 13_calculations/assi2.py
+++ 13_calculations/assi2.py
@@ -1,18 +1,16 @@
 import math
 num = int(input("Enter Number = "))
-a = num+1
+num = num+1
 
 while True:
-    j=2
-    while j<int(math.sqrt(a)):
-        if a%j==0:
+    i=2
+    for i in range(i,(num//2)+1):
+        if num%i==0:
             break
-        j+=1
-    if j>a//2:
-        print(j)
-        break
-    j+=1
-            
+        else:
+            print(num)
+    num+=1
+    break
 
  
 
