32897e4130247c3715ba7e811440c346f830e056
Anil Yadav <ay5741432@gmail.com>
2026-04-30 11:01:00 +0530

    Adding Remaining Operators in Day13 ClassWork Thursday

---
+++ Class_Work/Day13/in/1.py
@@ -0,0 +1,4 @@
+name="Anil"
+print("A" in name)  #True
+print("a"  in name) #False
+print("Ani" in name)#True
---
+++ Class_Work/Day13/in/2.py
@@ -0,0 +1,3 @@
+marks = [10, 20, 30, 40 ,50]
+print(30 in marks)
+print(20 not in marks)
