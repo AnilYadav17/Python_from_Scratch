faf9f11918c947c1c74abd0e0f75452b610ae829
Anil Yadav <ay5741432@gmail.com>
2026-05-14 01:26:02 +0530

    Adding Number30-40 Assignments in 13_May Assignments

--- 23_13May/assi36.py
+++ 23_13May/assi36.py
@@ -1,15 +1,15 @@
-'''35.
-*****
-*  *
-* *
-**
-*'''
+'''36.
+ABCDE
+A  D
+A C
+AB
+A'''
 
 num = int(input("Enter Number: "))
 for i in range(num,0,-1):
     for j in range(1,i+1):
         if j==1 or i==num or j==i:
-            print("*",end="")
+            print(chr(ord("A")+j-1),end="")
         else:
             print(" ",end="")
     print()
--- 23_13May/assi37.py
+++ 23_13May/assi37.py
@@ -1,15 +1,16 @@
-'''35.
+'''37.
 *****
-*  *
-* *
-**
-*'''
+####
+***
+##
+*
+'''
 
 num = int(input("Enter Number: "))
 for i in range(num,0,-1):
     for j in range(1,i+1):
-        if j==1 or i==num or j==i:
-            print("*",end="")
+        if i%2==0:
+            print("#",end="")
         else:
-            print(" ",end="")
+            print("*",end="")
     print()
