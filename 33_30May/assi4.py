171c3ce6c1c1858528601c94e3074f94965f3850
Anil Yadav <ay5741432@gmail.com>
2026-05-31 10:05:15 +0530

    Updating assignment 4 for 30May

--- 33_30May/assi4.py
+++ 33_30May/assi4.py
@@ -35,20 +35,60 @@ for i in range(n):
     x=int(input(f"{i+1}. Elemenet: "))
     arr1.append(x)
 arr=arr1.copy()
-#count
-count=1
-if n>1:
-    for i in range(len(arr)):
-        if i==0:
-            if arr[i]+1==arr[i+1]:
-                count+=1
-        elif i==len(arr)-1:
-            if arr[i]==arr[i-1]+1:
-                count+=1
-        else:
-            if arr[i]+1==arr[i+1]:
-                count+=1
-            else:
-                break
+arr1.sort()
 
+count=1
+flag=True
+for i in range(len(arr1)):
+    if i==0:
+        if arr1[i]+1==arr1[i+1]:
+            count+=1
+        else:
+            flag=False
+            break
+    elif i==len(arr1)-1:
+        pass
+    #     if arr1[i]-1==arr1[i-1]:
+    #         count=+1
+    #     else:
+    #         flag=False
+    else:
+        if arr1[i]+1==arr1[i+1]:
+            count+=1
+        else:
+            break
 print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #count
+# count=1
+# if n>1:
+#     for i in range(len(arr)):
+#         if i==0:
+#             if arr[i]+1==arr[i+1]:
+#                 count+=1
+#         elif i==len(arr)-1:
+#             if arr[i]==arr[i-1]+1:
+#                 count+=1
+#         else:
+#             if arr[i]+1==arr[i+1]:
+#                 count+=1
+#             else:
+#                 break
+# print(count)
+
+
