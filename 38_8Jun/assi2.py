a4992e31977c63a0f0c1774427b2b15be387e246
Anil Yadav <ay5741432@gmail.com>
2026-06-08 21:24:08 +0530

    Adding Assignment 2 and Assignment3 for 8Jun

---
+++ 38_8Jun/assi2.py
@@ -0,0 +1,97 @@
+'''2.
+=========================================
+ONLINE COURSE ENROLLMENT SYSTEM
+=========================================
+
+An institute offers:
+1. Python Course
+2. Java Course
+
+Store enrolled student email IDs using sets.
+
+Menu:
+1. Enroll Student in Python
+2. Enroll Student in Java
+3. Display Python Students
+4. Display Java Students
+5. Find Students Enrolled in Both Courses
+6. Find Students Enrolled Only in Python
+7. Find Students Enrolled Only in Java
+8. Check Enrollment in Python Course
+9. Display Total Unique Students
+10. Exit
+
+Requirements:
+- Use two sets.
+- Use membership operator (in).
+- Use union, intersection and difference operations.'''
+
+python_students=set()
+java_students=set()
+
+while True:
+    print("""\n=========================================
+ONLINE COURSE ENROLLMENT SYSTEM
+=========================================
+Menu:
+1. Enroll Student in Python
+2. Enroll Student in Java
+3. Display Python Students
+4. Display Java Students
+5. Find Students Enrolled in Both Courses
+6. Find Students Enrolled Only in Python
+7. Find Students Enrolled Only in Java
+8. Check Enrollment in Python Course
+9. Display Total Unique Students
+10. Exit
+""")
+    choice = int(input("Enter Choice: "))
+    match choice:
+        case 1:
+            n = int(input("Enter number of Students: "))
+            for i in range(n):
+                python_students.add(input(f"Enter name of Student{i+1}: "))
+
+        case 2:
+            n = int(input("Enter number of Students: "))
+            for i in range(n):
+                java_students.add(input(f"Enter name of Student{i+1}: "))
+        
+        case 3:
+            print("\nPython Students: ")
+            for i in python_students:
+                print(i,end=" ")
+        
+        case 4:
+            print("\nJava Students: ")
+            for i in java_students:
+                print(i,end=" ")
+            
+        case 5:
+            print("\nStudents enrolled in both courses: ")
+            for i in  python_students.intersection(java_students):
+                print(i,end=" ")
+            
+        case 6:
+            print("\nStudents enrolled in only Python: ")
+            for i in  python_students.difference(java_students):
+                print(i,end=" ")
+              
+        case 7:
+            print("\nStudents enrolled in only Java: ")
+            for i in  java_students.difference(python_students):
+                print(i,end=" ")
+
+        case 8:
+            i = input("\nStudents name to check Enrollment: ")
+            if i in python_students:
+              print("Student Enrolled")
+            else:
+              print("Student Not Enrolled")
+        
+        case 9:
+            print(f"\nTotal unique students are : {len(python_students.union(java_students))}")
+        
+        case 10:
+            print("Thanks for visiting ONLINE COURSE MANAGEMENT SYSTEM")
+            break
---
+++ 38_8Jun/assi3.py
@@ -0,0 +1,76 @@
+'''3.
+=========================================
+WEBSITE VISITOR TRACKING SYSTEM
+=========================================
+
+A website stores unique visitor IDs.
+
+Menu:
+1. Add Visitor
+2. Remove Visitor
+3. Check Visitor
+4. Display All Visitors
+5. Count Unique Visitors
+6. Clear Visitor Data
+7. Exit
+
+Requirements:
+- Use a set to store visitor IDs.
+- Duplicate visitor IDs should not be stored.
+- Use add(), remove(), and membership operations.'''
+
+visitor=set()
+
+while True:
+  print('''\n
+=========================================
+WEBSITE VISITOR TRACKING SYSTEM
+=========================================
+Menu:
+1. Add Visitor
+2. Remove Visitor
+3. Check Visitor
+4. Display All Visitors
+5. Count Unique Visitors
+6. Clear Visitor Data
+7. Exit''')
+  c = int(input("Enter choice: "))
+
+  match c:
+    case 1:
+      n = int(input("Enter number of visitors: "))
+      for i in range(n):
+        visitor.add(input(f"Enter {i+1} visitor name: "))
+      
+    case 2:
+      t = input("Enter name of visitor: ")
+      for i in visitor:
+        if i==t:
+          visitor.remove(i)
+          print(f"Successfully removed {t}")
+          break
+        else:
+          print(f"{t} not found in Visitors list")
+    
+    case 3:
+      t = input("Enter name of visitor: ")
+      if t in visitor:
+        print("Valid visitor")
+      else:
+        print("Visitor not found")
+    
+    case 4:
+      for i in visitor:
+        print(i,end=" ")
+      
+    case 5:
+      print(f"Total unique elements are {len(visitor)}")
+
+    case 6:
+      visitor.clear()
+      print("There is no visitor left")
+
+    case 7:
+      print("Thanks for visiting VISITOR TRACKING SYSTEM ")
+      break
+        
