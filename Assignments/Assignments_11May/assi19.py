00fec2d9134edd62469ba4d71cf609f2e7472196
Anil Yadav <ay5741432@gmail.com>
2026-06-12 11:18:42 +0530

    Adding Functions to o main Branch

---
+++ Assignments/Assignments_11May/assi19.py
@@ -0,0 +1,17 @@
+'''19) Reverse Number Cross
+    5   5
+     4 4
+      3
+     4 4
+    5   5'''
+
+
+num = int(input("Enter Number: "))
+for i in range(1,num+1):
+    print()
+    for j in range(1,num+1):
+        if i==1 and j==1 or i==1 and  j==num  or j==1 and i==num or j==num and i==num:
+            print(num,end="")
+        elif i==num//2
+        else:
+            print(" ",end="") 
---
+++ Functions/Demo/1.py
@@ -0,0 +1,6 @@
+#Function should be defined firstly -> here we can not call hello() , before creation 
+
+def hello():
+    print("First Demo Function")
+
+hello()
