7969d5901bcc7c3e147919489387b7d580e8a1a8
Anil Yadav <ay5741432@gmail.com>
2026-06-27 03:58:39 +0530

    Move basic module examples into Modules

--- 03_Modules&Packages/ClassWork/01/module2.py
+++
@@ -1,9 +0,0 @@
-#Please  ensure Module1 ,Module2 ,MOdule3 exist in same directory
-
-#from module1 import add
-#import module1 as m1
-from  module1 import func,add
-from module3 import func    #here module3 will win 
-print(add(1,2))
-print(func())
-
--- 03_Modules&Packages/ClassWork/01/module3.py
+++
@@ -1,6 +0,0 @@
-#Please  ensure Module1 ,Module2 ,MOdule3 exist in same directory
-
-
-
-def func():
-	return "Function in Module3"
