d7c55579e0abbc4c3129b746d4141c6e37a63bf3
Anil Yadav <ay5741432@gmail.com>
2026-06-26 17:07:45 +0530

    Add examples for importing modules from different directories

---
+++ 03_Modules/ClassWork/26Jun_Modules/04_Main_Anywhere/main/main.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append("/home/aniiil/Desktop/InfoBeans/Python_from_Scratch/03_Modules/ClassWork/26Jun_Modules/04")
+
+from mypackages import module1,module2
+module1.func1()
+module2.func2()
---
+++ 03_Modules/ClassWork/26Jun_Modules/04_Main_Anywhere/mypackages/module1.py
@@ -0,0 +1,2 @@
+def func1():
+	print("Fuction1 from Module1")
