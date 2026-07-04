ecbb33209c74065cfe3c8f1e9846b6fd9c467274
Anil Yadav <ay5741432@gmail.com>
2026-07-05 03:49:30 +0530

    Compliting Answer for 2Assignment

--- 04_OOPs/Assignments/02_3Jul/Solution.py
+++ 04_OOPs/Assignments/02_3Jul/Solution.py
@@ -12,35 +12,32 @@ class Customer:
         
 
     def deposit(self,amount):
-        self.deposit = amount
-        self.balance+=self.deposit
+        self.balance += amount
 
     def withdraw(self,amount):
-        self.withdraw = amount
-        self.balance-=self.withdraw
+        self.balance -= amount
     
-    def transfer_money(self,reciever, amount):
-        self.reciever = reciever
-        self.transffered = amount
-        self.balance-=self.transffered
+    def transfer_money(self, receiver, amount):
+        self.balance -= amount
+        receiver.balance += amount
 
     def display_balance(self):
         return self.balance
     
     @classmethod
     def change_interest_rate(cls,new_rate):
-        cls.new_interest_rate = new_rate 
+        cls.Interest_Rate = new_rate 
 
     @classmethod
     def change_bank_name(cls,new_name):
-        cls.new_bank_name = new_name
+        cls.Bank_Name = new_name
 
     @classmethod
     def display_bank_info(cls):
-        return cls.new_bank_name,cls.new_interest_rate
+        return cls.Bank_Name, cls.Interest_Rate
 
     @staticmethod
-    def validate_account_number(self,account_no):
+    def validate_account_number(account_no):
         if account_no in Customer.accounts:
             return True
         else:
@@ -55,20 +52,28 @@ class Customer:
         Customer.transaction_counter+=1
         return f"TXN{Customer.transaction_counter}"
     
+for i in range(2):
+    print(f"Customer{i+1}")
+    ac_no = int(input("Account No : "))
+    name = input("Customer Name : ")
+    balance = float(input("Account Balance : "))
+    globals()[f"c{i+1}"] = Customer(ac_no,name,balance)
 
 
-c1 = Customer(1001,"Anil",50000)
 c2 = Customer(1002,"Anu",30000)
 
 amount =  float(input("Enter Deposit Amount: "))
 c1.deposit(amount)
 
 amount = float(input("Enter Transfer Amount: "))
-receiver = c2.account_no
-c1.transfer_money(receiver,amount)
+c1.transfer_money(c2, amount)
 
+print(f"Customer : {c1.customer_name}")
+print(f"Balance  : {c1.display_balance()}\n")
 
-print(c1.display_balance())
-c2.balance + c2.calculate_interest(c2.balance,c2.Interest_Rate)
+print(f"Customer : {c2.customer_name}")
+print(f"Balance  : {c2.display_balance()}\n")
 
-print(c2.display_balance())
+print(f"Bank Name      : {Customer.Bank_Name}")
+print(f"Interest Rate  : {Customer.Interest_Rate}%")
+print(f"Transaction ID : {Customer.generate_transaction_id()}")
